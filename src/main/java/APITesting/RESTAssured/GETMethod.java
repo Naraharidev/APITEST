82e9711c4cc40f1f74d400e20c54e1aa10355e98
Narahari <naraharidevarapu@gmail.com>
2022-11-16 14:51:44 +0530

    COMMIT1

---
+++ src/main/java/APITesting/RESTAssured/GETMethod.java
@@ -0,0 +1,11 @@
+package APITesting.RESTAssured;
+
+import org.testng.annotations.Test;
+
+public class GETMethod {
+	@Test
+	public void getMethod() {
+		//Response res = RestAssured.get("https://dog.ceo/api/breeds/image/random");
+	}
+
+}
